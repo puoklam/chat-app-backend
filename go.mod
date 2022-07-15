af1dce8503634e6e518202071e43fa9814cef295
puoklam <puoklam0@gmail.com>
2022-07-15 14:13:21 +0000

    noti: remove pkg

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/puoklam/chat-app-backend
 go 1.18
 
 require (
-	firebase.google.com/go v3.13.0+incompatible
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt/v4 v4.4.1
@@ -12,24 +11,12 @@ require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/nsqio/go-nsq v1.1.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
-	google.golang.org/api v0.86.0
 	gorm.io/driver/postgres v1.3.7
 	gorm.io/gorm v1.23.5
 )
 
 require (
-	cloud.google.com/go v0.102.1 // indirect
-	cloud.google.com/go/compute v1.7.0 // indirect
-	cloud.google.com/go/firestore v1.6.1 // indirect
-	cloud.google.com/go/iam v0.3.0 // indirect
-	cloud.google.com/go/storage v1.23.0 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
-	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
-	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -40,14 +27,5 @@ require (
 	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	golang.org/x/oauth2 v0.0.0-20220630143837-2104d58473e0 // indirect
-	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220624142145-8cd45d7dbd1f // indirect
-	google.golang.org/grpc v1.47.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,62 +1,62 @@
 package notifications
 
-import (
-	"context"
-	"log"
+// import (
+// 	"context"
+// 	"log"
 
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/messaging"
-	"github.com/puoklam/chat-app-backend/env"
-	"google.golang.org/api/option"
-)
+// 	firebase "firebase.google.com/go"
+// 	"firebase.google.com/go/messaging"
+// 	"github.com/puoklam/chat-app-backend/env"
+// 	"google.golang.org/api/option"
+// )
 
-var app *firebase.App
+// var app *firebase.App
 
-var client *messaging.Client
+// var client *messaging.Client
 
-func init() {
-	app, err := firebase.NewApp(context.Background(), nil, option.WithCredentialsFile(env.FIREBASE_CRED_PATH))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	client, err = app.Messaging(context.Background())
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+// func init() {
+// 	app, err := firebase.NewApp(context.Background(), nil, option.WithCredentialsFile(env.FIREBASE_CRED_PATH))
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	client, err = app.Messaging(context.Background())
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// }
 
-func Send(ctx context.Context, data map[string]string, token string) (string, error) {
-	return client.Send(ctx, &messaging.Message{
-		Data:  data,
-		Token: token,
-	})
-}
+// func Send(ctx context.Context, data map[string]string, token string) (string, error) {
+// 	return client.Send(ctx, &messaging.Message{
+// 		Data:  data,
+// 		Token: token,
+// 	})
+// }
 
-func SendMultiCast(ctx context.Context, data map[string]string, tokens []string) (*messaging.BatchResponse, error) {
-	return client.SendMulticast(ctx, &messaging.MulticastMessage{
-		Data:   data,
-		Tokens: tokens,
-	})
-}
+// func SendMultiCast(ctx context.Context, data map[string]string, tokens []string) (*messaging.BatchResponse, error) {
+// 	return client.SendMulticast(ctx, &messaging.MulticastMessage{
+// 		Data:   data,
+// 		Tokens: tokens,
+// 	})
+// }
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// // // Import the functions you need from the SDKs you need
+// // import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// // // TODO: Add SDKs for Firebase products that you want to use
+// // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDh1f4Zyqdh76BijMRjcSpPH_JQmN7_fkg",
-//   authDomain: "im-push-notification.firebaseapp.com",
-//   projectId: "im-push-notification",
-//   storageBucket: "im-push-notification.appspot.com",
-//   messagingSenderId: "429729531661",
-//   appId: "1:429729531661:web:742e194242404f21d05244",
-//   measurementId: "G-YT18YSV23R"
-// };
+// // // Your web app's Firebase configuration
+// // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyDh1f4Zyqdh76BijMRjcSpPH_JQmN7_fkg",
+// //   authDomain: "im-push-notification.firebaseapp.com",
+// //   projectId: "im-push-notification",
+// //   storageBucket: "im-push-notification.appspot.com",
+// //   messagingSenderId: "429729531661",
+// //   appId: "1:429729531661:web:742e194242404f21d05244",
+// //   measurementId: "G-YT18YSV23R"
+// // };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// // // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
